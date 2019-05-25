5aee71fd8b93e4b7859908eaf934f7d33e5d02d1
suchang <1103232282@qq.com>
2019-05-25 15:25:09 +0800

    123

---
+++ leetcode/letter_combinations_of_a_phone_number.py
@@ -0,0 +1,32 @@
+from typing import *
+
+lookup = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = list()
+        if not digits:
+            return res
+        def slove(tmp_res:str, flag, alpha):
+            nonlocal res
+            tmp_res = tmp_res + alpha
+            if flag == len(digits) - 1:
+                res.append(tmp_res)
+                return
+            else:
+                next_d = lookup[digits[flag + 1]]
+                for a in next_d:
+                    slove(str(tmp_res), flag + 1, a)
+            return
+        for a in  lookup[digits[0]]:
+            slove(list(), 0, a)
+        return res
