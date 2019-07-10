cf6e96dc4ea7ba909afaa384aa80e04d13e9ffeb
suchang <1103232282@qq.com>
2019-07-10 10:46:38 +0800

    c++ 对象模型

--- leetcode/path_sum_ii.py
+++ leetcode/path_sum_ii.py
@@ -1,18 +1,32 @@
-class Solution:
-    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
-        res = []
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
-        def dfs(node, p:list, num):
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        _max = None
+        cache = list()
+
+        def get_max(node):
+            nonlocal _max
             if not node:
-                return
-            nonlocal res
-            p.append(node.val)
-            if num + node.val == sum:
-                if (node.right is None) and (node.left is None):
-                    res.append(p)
-                    return
-            num = num + node.val
-            dfs(node.left, list(p), num)
-            dfs(node.right, list(p), num)
-        dfs(root, [], 0)
-        return res
+                return 0
+            left_v = get_max(node.left)
+            right_v = get_max(node.right)
+            # 1 要
+            res = max(left_v + node.val, right_v + node.val, node.val)
+            if _max is None:
+                _max = res
+            _max = max(left_v + right_v + node.val, res, _max)
+            return res
+
+        get_max(root)
+
+        if _max is None:
+            return 0
+        else:
+            return _max
+
